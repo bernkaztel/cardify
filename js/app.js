ecd741a23cbbdce351d279920f91de4658ee9f1a
bernkaztel <bernkaztel@hotmail.com>
2018-02-28 12:36:12 -0600

    Adding my solution JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-Escribe tu código aqui----
+const galleryContainer = document.getElementById("gallery");
+
+
+const changeToFigure = gallery => {
+    const images = gallery.getElementsByTagName("img");
+    const arrayImages = Array.prototype.slice.call(images);
+    arrayImages.forEach(function (img) {
+        const imgSRC = img.getAttribute("src");
+        const imgALT = img.getAttribute("alt");
+        var figTemplate = `<figure><img src=${imgSRC}></img><figcaption>${imgALT}</figcaption></figure>`;
+        img.outerHTML = figTemplate;
+    });
+};
+changeToFigure(galleryContainer);
